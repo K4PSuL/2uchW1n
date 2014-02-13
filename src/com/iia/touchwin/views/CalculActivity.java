3c496f5aabe1a11dd9ef6a2549a5552af2ac0c59
Ch0uk <aurelie.tremblays@gmail.com>
2014-02-13 19:59:48 +0100

    CalculActivity + SettingsActivity (bouton Aide)

--- src/com/iia/touchwin/views/CalculActivity.java
+++ src/com/iia/touchwin/views/CalculActivity.java
@@ -7,71 +7,76 @@ import android.os.AsyncTask;
 import android.os.Bundle;
 import android.os.SystemClock;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.Button;
 import android.widget.TextView;
 
 import com.iia.touchwin.R;
-import com.iia.touchwin.entities.GameReflex;
+import com.iia.touchwin.entities.Game;
 import com.iia.touchwin.entities.Player;
 import com.iia.touchwin.utils.Const;
 import com.iia.touchwin.utils.Utils;
 
 public class CalculActivity extends Activity {
+	
 	private Player thePlayer;
 	private Player oPlayer2;
 	private TextView lbScoreP1;
 	private TextView lbScoreP2;
+	private Player oPlayerWinner;
+	private Button btnP1;
+	private Button btnP2;
+	private TextView lbChrono;
+	private Animation animateChrono;
+	private int nbRounds;
+	private boolean play;
+	private boolean isTrue;
+	private Game oGame;
+	private int roundPlay = 0;
 	private int calcul;
 	private TextView lbCalculP1;
 	private TextView lbCalculP2;
 	private int result;
-	private int nbRounds;
-	private boolean play;
-	private boolean isTrue;
-	private Player oPlayerWinner;
-	private Button btnP1;
-	private Button btnP2;
 	
+	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 
 		setContentView(R.layout.activity_game);
 
+		lbChrono = (TextView) findViewById(R.id.txtChrono);
 		btnP1 = (Button) findViewById(R.id.btnP1);
 		btnP2 = (Button) findViewById(R.id.btnP2);
-
-		//imgColor = (ImageView) findViewById(R.id.imgColor);
-		
+		lbScoreP1 = (TextView) findViewById(R.id.lbScoreP1);
+		lbScoreP2 = (TextView) findViewById(R.id.lbScoreP2);
 		lbCalculP1 = (TextView) findViewById(R.id.lbCalculP1);
 		lbCalculP2 = (TextView) findViewById(R.id.lbCalculP2);
+		
+		animateChrono = AnimationUtils.loadAnimation(CalculActivity.this,
+				R.anim.chrono);
+		
+		oGame = (Game) getIntent().getExtras().getSerializable(
+				Const.BUNDLE_GAME);
+		
+		nbRounds = getIntent().getExtras().getInt(Const.BUNDLE_TIME);
 		
 		thePlayer = (Player) getIntent().getExtras().getSerializable(
 				Const.BUNDLE_PLAYER);
 
 		oPlayer2 = (Player) getIntent().getExtras().getSerializable(
 				Const.BUNDLE_PLAYER2);
-		
-		nbRounds = getIntent().getExtras().getInt(Const.BUNDLE_TIME);
-		
+
 		calcul = 0;
 		result = 0;
 		isTrue = false;
 		
-		lbScoreP1 = (TextView) findViewById(R.id.lbScoreP1);
-		lbScoreP2 = (TextView) findViewById(R.id.lbScoreP2);
-
 		btnP1.setText(thePlayer.getLogin());
 		btnP2.setText(oPlayer2.getLogin());
-		
-		Utils.playSound(CalculActivity.this, CalculActivity.this, R.raw.five);
-		Utils.playSound(CalculActivity.this, CalculActivity.this, R.raw.four);
-		Utils.playSound(CalculActivity.this, CalculActivity.this, R.raw.three);
-		Utils.playSound(CalculActivity.this, CalculActivity.this, R.raw.two);
-		Utils.playSound(CalculActivity.this, CalculActivity.this, R.raw.one);
 
-		// On éxecute le jeux dans un autre thread
-		GameAsyncTask inGame = new GameAsyncTask();
-		inGame.execute();
+		// On démarre le chronomètre
+		ChronoAsyncTask chrono = new ChronoAsyncTask();
+		chrono.execute();
 
 		btnP1.setOnClickListener(new View.OnClickListener() {
 			
@@ -81,185 +86,249 @@ public class CalculActivity extends Activity {
 				if (play) {
 					play = false;
 
+					// Si il fallait cliquer
 					if (isTrue) {
 						lbScoreP1.setText(String.valueOf(Integer
 								.valueOf((String) lbScoreP1.getText()) + 1));
+
 						btnP1.setBackgroundColor(R.color.green);
-						Utils.playSound(CalculActivity.this, CalculActivity.this, R.raw.win);
+
+						Utils.playSound(CalculActivity.this, CalculActivity.this,
+								R.raw.win);
 					} else {
 						lbScoreP2.setText(String.valueOf(Integer
 								.valueOf((String) lbScoreP2.getText()) + 1));
+
 						btnP1.setBackgroundColor(R.color.red);
-						Utils.playSound(CalculActivity.this, CalculActivity.this, R.raw.loose);
+
+						Utils.playSound(CalculActivity.this, CalculActivity.this,
+								R.raw.loose);
 					}
 
-					//imgColor.setBackgroundResource(R.color.gray);
+					roundPlay++;
+					
+					//imgColor.setBackground(null);
 				}
 			}
 		});
 
 		btnP2.setOnClickListener(new View.OnClickListener() {
-			
+
 			@SuppressLint("ResourceAsColor")
 			@Override
 			public void onClick(View v) {
 				if (play) {
 					play = false;
 
+					// Si il fallait cliquer
 					if (isTrue) {
 						lbScoreP2.setText(String.valueOf(Integer
 								.valueOf((String) lbScoreP2.getText()) + 1));
+
 						btnP2.setBackgroundColor(R.color.green);
-						Utils.playSound(CalculActivity.this, CalculActivity.this, R.raw.win);
+
+						Utils.playSound(CalculActivity.this, CalculActivity.this,
+								R.raw.win);
 					} else {
 						lbScoreP1.setText(String.valueOf(Integer
 								.valueOf((String) lbScoreP1.getText()) + 1));
-						btnP2.setBackgroundColor(R.color.red);
-						Utils.playSound(CalculActivity.this, CalculActivity.this, R.raw.loose);
-					}
 
-					//imgColor.setBackgroundResource(R.color.gray);
+						btnP2.setBackgroundColor(R.color.red);
+
+						Utils.playSound(CalculActivity.this, CalculActivity.this,
+								R.raw.loose);
+					}
+					
+					roundPlay++;
+					
+					//imgColor.setBackground(null);
 				}
 			}
+
 		});
 	}
-
+	
+	private void calcul(int min, int max) {
+		int cpt = Utils.randomTime(1, 3); // bon ou mauvais calcul
+		result = calcul;
+		
+		if (cpt == 1) {
+			isTrue = true;
+		} else {
+			do {
+				result = Utils.randomTime(calcul-5, calcul+5);
+			} while (result == calcul);
+			isTrue = false;
+		}
+	}
+	
 	private void setNewCalcul() {
-		int nb1 = GameReflex.randomTime(0, 100); // premier nombre
-		int nb2 = GameReflex.randomTime(0, 100); // deuxième nombre
-		int operator = GameReflex.randomTime(1, 4); // opérateur
-		int cpt; // bon ou mauvais calcul
-		String lbCalcul = "";
+		int nb1 = Utils.randomTime(0, 26); // premier nombre
+		int nb2 = Utils.randomTime(0, 26); // deuxième nombre
+		int operator = Utils.randomTime(1, 4); // opérateur
+		String lbCalcul = ""; // libellé du calcul à afficher
 		
 		switch (operator) {
 		case 1:
 			calcul = nb1 + nb2;
-			cpt = GameReflex.randomTime(1, 3);
-			result = calcul;
-			
-			if (cpt == 1) {
-				isTrue = true;
-			} else {
-				do {
-					result = GameReflex.randomTime(calcul-5, calcul+5);
-				} while (result == calcul);
-				isTrue = false;
-			}
-			
+			calcul(0,26);			
 			lbCalcul = nb1 + " + " + nb2 + " = " + result;
 			break;
 		case 2:
 			calcul = nb1 - nb2;
-			cpt = GameReflex.randomTime(1, 3);
-			result = calcul;
-			
-			if (cpt == 1) {
-				isTrue = true;
-			} else {
-				do {
-					result = GameReflex.randomTime(calcul-5, calcul+5);
-				} while (result == calcul);
-				isTrue = false;
-			}
-			
+			calcul(0,26);
 			lbCalcul = nb1 + " - " + nb2 + " = " + result;
 			break;
 		case 3:
 			calcul = nb1 * nb2;
-			cpt = GameReflex.randomTime(1, 3);
-			result = calcul;
-			
-			if (cpt == 1) {
-				isTrue = true;
-			} else {
-				do {
-					result = GameReflex.randomTime(calcul-5, calcul+5);
-				} while (result == calcul);
-				isTrue = false;
-			}
-			
+			calcul(0,11);
 			lbCalcul = nb1 + " x " + nb2 + " = " + result;
 			break;
 		default:
 			break;
 		}
 		
-		lbCalculP1.setVisibility(View.VISIBLE);
-		lbCalculP2.setVisibility(View.VISIBLE);
 		lbCalculP1.setText(lbCalcul);
 		lbCalculP2.setText(lbCalcul);
 	}
 	
 	private class GameAsyncTask extends AsyncTask<Void, Integer, Void> {
-		protected void onProgressUpdate(Integer... randomInteger) {
-			super.onProgressUpdate(randomInteger);
 
-			btnP1.setBackgroundResource(R.color.gray);
-			btnP2.setBackgroundResource(R.color.gray);
+		protected void onProgressUpdate(Integer... values) {
+			super.onProgressUpdate(values);
 
-			lbCalculP1.setText(" ");
-			lbCalculP2.setText(" ");
-			
-			lbCalculP1.setVisibility(View.INVISIBLE);
-			lbCalculP2.setVisibility(View.INVISIBLE);
+			lbCalculP1.setText(null);
+			lbCalculP2.setText(null);
 			
 			setNewCalcul();
 			
 			play = true;
+
+			if (nbRounds == roundPlay - 1) {
+				this.cancel(true);
+			}
 		}
 
 		protected Void doInBackground(Void... arg0) {
-			int roundPlay = 0;
-			
 			do {
-				roundPlay++;
+				SystemClock.sleep(5000);
 
-				SystemClock.sleep(GameReflex.randomTime(2, 10) * 1000);
+				publishProgress(Utils.randomTime(1, 3));
 
-				publishProgress(GameReflex.randomTime(1, 4));
-
-			} while (nbRounds != roundPlay);
-
-			return null;
+			} while (true != false);
 		}
-		
-		protected void onPostExecute(Void result) {
+
+		@Override
+		protected void onCancelled() {
 			final Dialog oDialogEndGame = new Dialog(CalculActivity.this);
-		
+
 			int scoreP1 = Integer.valueOf((String) lbScoreP1.getText());
 			int scoreP2 = Integer.valueOf((String) lbScoreP2.getText());
-			
+
 			if (scoreP1 > scoreP2) {
 				oPlayerWinner = thePlayer;
 			} else {
 				oPlayerWinner = oPlayer2;
 			}
-			
+
 			oDialogEndGame.setContentView(R.layout.dialog_game_end);
-			oDialogEndGame.setTitle(oPlayerWinner.getLogin() + " à gagné !");
-			
-			TextView txtPseudo = (TextView) oDialogEndGame
+			oDialogEndGame.setTitle(oPlayerWinner.getLogin() + Const.WINNER);
+
+			TextView lbPseudo = (TextView) oDialogEndGame
 					.findViewById(R.id.txtPseudo);
-			TextView txtScore = (TextView) oDialogEndGame
+			TextView lbScore = (TextView) oDialogEndGame
 					.findViewById(R.id.txtScore);
 			Button btnExitEndGame = (Button) oDialogEndGame
 					.findViewById(R.id.btnExitEndGame);
-			
-			txtPseudo.setText(oPlayerWinner.getLogin());
-			txtScore.setText(lbScoreP1.getText() + " - " + lbScoreP2.getText());
+
+			lbPseudo.setText(oPlayerWinner.getLogin());
+			lbScore.setText(lbScoreP1.getText() + " - " + lbScoreP2.getText());
 
 			oDialogEndGame.show();
 
 			Utils.playSound(CalculActivity.this, CalculActivity.this, R.raw.end);
-			
+
+			Utils.saveScore(thePlayer, oPlayer2, scoreP1, scoreP2, oGame,
+					CalculActivity.this);
+
 			btnExitEndGame.setOnClickListener(new View.OnClickListener() {
-				
+
 				@Override
 				public void onClick(View v) {
 					oDialogEndGame.dismiss();
 				}
 			});
-			
 		}
+	}
+
+	private class ChronoAsyncTask extends AsyncTask<Void, Integer, Void> {
+		
+		protected void onProgressUpdate(Integer... number) {
+			super.onProgressUpdate(number);
+
+			switch (number[0]) {
+			case 5:
+				Utils.playSound(CalculActivity.this, CalculActivity.this,
+						R.raw.five);
+				lbChrono.setText(String.valueOf(number[0]));
+				break;
+			case 4:
+				Utils.playSound(CalculActivity.this, CalculActivity.this,
+						R.raw.four);
+				lbChrono.setText(String.valueOf(number[0]));
+				break;
+			case 3:
+				Utils.playSound(CalculActivity.this, CalculActivity.this,
+						R.raw.three);
+				lbChrono.setText(String.valueOf(number[0]));
+				break;
+			case 2:
+				Utils.playSound(CalculActivity.this, CalculActivity.this, 
+						R.raw.two);
+				lbChrono.setText(String.valueOf(number[0]));
+				break;
+			case 1:
+				Utils.playSound(CalculActivity.this, CalculActivity.this, 
+						R.raw.one);
+				lbChrono.setText(String.valueOf(number[0]));
+				break;
+			case 0:
+				lbChrono.setText("Go !");
+				break;
+			default:
+				lbChrono.setText(null);
+				break;
+			}
+
+			lbChrono.startAnimation(animateChrono);
+
+		}
+
+		protected Void doInBackground(Void... arg0) {
+
+			publishProgress(5);
+			SystemClock.sleep(1000);
+			publishProgress(4);
+			SystemClock.sleep(1000);
+			publishProgress(3);
+			SystemClock.sleep(1000);
+			publishProgress(2);
+			SystemClock.sleep(1000);
+			publishProgress(1);
+			SystemClock.sleep(1000);
+			publishProgress(0);
+			SystemClock.sleep(500);
+			publishProgress(-1);
+
+			return null;
+		}
+
+		protected void onPostExecute(Void result) {
+			// On éxecute le jeu dans un autre thread
+			GameAsyncTask inGame = new GameAsyncTask();
+			inGame.execute();
+
+		}
+
 	}
 }
--- src/com/iia/touchwin/views/SettingsActivity.java
+++ src/com/iia/touchwin/views/SettingsActivity.java
@@ -7,9 +7,11 @@ import com.iia.touchwin.utils.Utils;
 
 import android.app.Activity;
 import android.content.Context;
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.CheckBox;
 
 public class SettingsActivity extends Activity {
@@ -19,6 +21,7 @@ public class SettingsActivity extends Activity {
 		setContentView(R.layout.activity_settings);
 
 		final CheckBox checkBoxSound = (CheckBox) findViewById(R.id.checkBoxSound);
+		final Button btnHelp = (Button) findViewById(R.id.btnHelp);
 		final Player thePlayer = (Player) getIntent().getExtras()
 				.getSerializable(Const.BUNDLE_PLAYER);
 
@@ -37,11 +40,25 @@ public class SettingsActivity extends Activity {
 
 				if (checkBoxSound.isChecked()) {
 					oEditor.putBoolean(Const.PREFERENCES_SOUND, true);
-					Utils.playSound(SettingsActivity.this, SettingsActivity.this, R.raw.switch_sound);
+					Utils.playSound(SettingsActivity.this, 
+							SettingsActivity.this, R.raw.switch_sound);
 				} else {
 					oEditor.putBoolean(Const.PREFERENCES_SOUND, false);
 				}
 				oEditor.commit();
+			}
+		});
+		
+		
+		btnHelp.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				/*
+				Intent intentOpenHelp = new Intent(SettingsActivity.this,
+						FragmentsSliderActivity.class);
+				
+				startActivity(intentOpenHelp);
+				*/
 			}
 		});
 
